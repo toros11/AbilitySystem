713a55b3d0425ec46f5a679ece05281b3c9171c4
Vagner Seibert <vagnerseibert@gmail.com>
2017-04-27 01:01:10 -0700

    added speed parameter to MoveToLocation action

--- Assets/Src/Engine/Intelligence/Actions/MoveToLocation.cs
+++ Assets/Src/Engine/Intelligence/Actions/MoveToLocation.cs
@@ -6,12 +6,14 @@ namespace Intelligence {
 
         public bool autobreak = true;
         private UnityEngine.AI.NavMeshAgent agent;
+        public float speed = 1;
 
         public override void OnStart() {
             Vector3 location = context.point;
             agent = context.entity.GetComponent<UnityEngine.AI.NavMeshAgent>();
             agent.autoBraking = autobreak;
             agent.SetDestination(location);
+            agent.speed = speed;
         }
 
         public override CharacterActionStatus OnUpdate() {
