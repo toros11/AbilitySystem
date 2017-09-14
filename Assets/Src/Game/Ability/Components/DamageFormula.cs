d957f0d5cd694239de71795b1dccaa0858c545e4
toros11 <tobias.rosen@axsh.net>
2017-09-14 15:11:54 +0900

    formulas: improve modification of base dice value

--- Assets/Src/Game/Ability/Components/DamageFormula.cs
+++ Assets/Src/Game/Ability/Components/DamageFormula.cs
@@ -15,36 +15,39 @@ public class DamageFormula : AbilityComponent<Context> {
 
     public override void OnUse() {
         var diceCreator = new DiceCreator();
-        var finalInput = FinalizeDice();
-        var sum = (float)diceCreator[finalInput].Result;
+        var finalizedInput = FinalizeDice((float)inputValue);
+        var sum = (float)diceCreator[finalizedInput].Result;
+
         for(int i = 0; i < modifiers.Count; i++) {
             var j = sum;
             modifiers[i].SetContext(this.context);
-            modifiers[i].ApplyModifier(ref sum);
-            if(debugMode) Debug.Log("Apply modifier:"+ modifiers[i].GetType() + " [ input value: " + j + " => " + sum + "]");
+            modifiers[i].ApplyModifier<Ability>(ability, ref sum);
+            if(debugMode)
+                Debug.Log("Apply modifier:"+ modifiers[i].GetType() + " [ input value: " + j + " => " + sum + "]");
         }
         outputValue = sum;
         // TODO: use this output in a way that makes more sense
     }
 
-    private DiceBase FinalizeDice() {
-        int newBase;
-        int sizeMod = 0;
+    private DiceBase FinalizeDice(float input) {
+        float newBase = input;
+        float sizeMod = 0;
 
         if(debugMode) {
             size.OnAfterDeserialize();
             inputFormula.OnAfterDeserialize();
         }
-        if (size.PointsToMethod) {
-            sizeMod = size.Invoke(this.context);
-        }
         if (inputFormula.PointsToMethod) {
-            newBase = (int)inputFormula.Invoke(this.context) + sizeMod;
-        } else {
-            newBase = (int)inputValue + sizeMod;
+            newBase = (float)inputFormula.Invoke(this.context);
         }
-
-        if(newBase <= 0) newBase = 0;
-        return (DiceBase)(int)Mathf.Max(0,Mathf.Min(newBase,(int)DiceBase.BASE_6d6));
+        if (size.PointsToMethod) {
+            newBase = input + size.Invoke(this.context);
+            if (input < (float)DiceBase.BASE_0) {
+                newBase = Mathf.Max(0,Mathf.Min(newBase,(float)DiceBase.BASE_6d6));
+            } else {
+                newBase = Mathf.Max((float)DiceBase.BASE_0,Mathf.Min(newBase,(float)DiceBase.BASE_10));
+            }
+        }
+        return (DiceBase)newBase;
     }
 }
